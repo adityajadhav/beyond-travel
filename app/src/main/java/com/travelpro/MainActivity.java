b80ee426f2901deb243395f69c589dbc2f9fa45d
Aditya Jadhav <aditya.jadhav27@gmail.com>
2017-02-25 18:00:34 -0600

    adding main activity

--- app/src/main/java/com/travelpro/MainActivity.java
+++ app/src/main/java/com/travelpro/MainActivity.java
@@ -1,13 +1,28 @@
 package com.travelpro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.travelpro.entities.User;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         super.onCreate(savedInstanceState);
+        writeNewUser("ajadhav4", "Aditya", "abcs@abc.com");
         setContentView(R.layout.activity_main);
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+        User user = new User(name, email);
+
+        mDatabase.child("users").child(userId).setValue(user);
     }
 }
